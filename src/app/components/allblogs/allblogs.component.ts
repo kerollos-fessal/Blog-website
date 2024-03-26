9908790407c8e2486745ad2dfd5221d648ca5451
Kerollos Fessal <kerofessal77@gmail.com>
2024-03-26 12:50:55 +0200

    project structure setup

---
+++ src/app/components/allblogs/allblogs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-allblogs',
+  templateUrl: './allblogs.component.html',
+  styleUrls: ['./allblogs.component.scss']
+})
+export class AllblogsComponent {
+
+}
