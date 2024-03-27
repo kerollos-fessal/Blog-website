36a4eb7ae65e1ba83d0a27563779c311c319199c
Kerollos Fessal <kerofessal77@gmail.com>
2024-03-27 16:58:50 +0200

    code structure optimization

--- src/app/components/blog-item/blog-item.component.ts
+++ src/app/components/blog-item/blog-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Blog } from 'src/app/interfaces/blog';
 import { BlogsService } from 'src/app/services/blogs.service';
@@ -8,37 +8,11 @@ import { BlogsService } from 'src/app/services/blogs.service';
   templateUrl: './blog-item.component.html',
   styleUrls: ['./blog-item.component.scss']
 })
-export class BlogItemComponent implements OnInit{
+export class BlogItemComponent{
+  @Input() blog?: Blog;
 
-  allBlogs: Blog[]= [];
-  limit:number =9
-  searchKey: string= '';
-  constructor(private _blogsService: BlogsService){
+  constructor(){
   }
 
-ngOnInit(): void {
-  this._blogsService.limit.subscribe((res)=>{
-    this.limit = res;
-    this.getAllBlogs();
-   });
-
-   this._blogsService.searchValue.subscribe((res)=>{
-    this.searchKey = res;
-   });
-
-}
-
-getAllBlogs(){
-this._blogsService.getBlogs(this.limit).subscribe({
-  next:(res)=>{
-    // console.log(res);
-    this.allBlogs = res;
-  },
-  error:(err)=>{
-    console.log(err);
-    
-  }
-})
-}
 
 }
